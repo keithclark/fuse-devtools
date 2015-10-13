4ec581a0a1c62abe37fd3b15860635b140d25863
Keith Clark <kclark@globalpersonals.co.uk>
2015-10-13 13:40:58 +0100

    Expose i18n to grunt config for Firefox manifests

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,7 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        i18n: grunt.file.readJSON('lang/en.json'),
         jshint: {
             options: {
                 strict: true,
